15d4ec7bde8f5f1d8597a79a88d67d27d786de92
quaner2557 <qq974807643@gmail.com>
2016-11-17 16:33:15 +0800

    opp fundamental part

--- pythontry/opp.py
+++ pythontry/opp.py
@@ -55,3 +55,84 @@ class Student(object):
 
 #####################################################################
 # 继承和多态
+# 定义一个class的时候，我们实际上就定义了一种数据类型
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    pass
+
+class Cat(Animal):
+    pass
+
+c = Cat()
+print(isinstance(c, Cat))
+print(isinstance(c, Animal))
+
+b = Animal()
+print(isinstance(b, Dog))
+print(isinstance(b, Animal))
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+run_twice(Dog())
+
+class Tortoise(Animal):
+    def run(self):
+        print('Tortoise is running slowly...')
+
+run_twice(Tortoise())
+
+#####################################################################
+# 使用type()判断对象类型
+print(type(1234))
+
+import types
+def fn():
+    pass
+
+print(type(fn) == types.FunctionType)
+
+# 或者用isinstance( , )
+# hasattr(obj, 'x') # 有属性'x'吗？
+# setattr(obj, 'y', 19) # 设置一个属性'y'
+# getattr(obj, 'y') # 获取属性'y'
+
+class MyObject(object):
+
+    def __init__(self):
+        self.x = 9
+
+    def power(self):
+        return self.x * self.x
+
+obj = MyObject()
+
+print('hasattr(obj, \'x\') =', hasattr(obj, 'x')) # 有属性'x'吗？
+print('hasattr(obj, \'y\') =', hasattr(obj, 'y')) # 有属性'y'吗？
+setattr(obj, 'y', 19) # 设置一个属性'y'
+print('hasattr(obj, \'y\') =', hasattr(obj, 'y')) # 有属性'y'吗？
+print('getattr(obj, \'y\') =', getattr(obj, 'y')) # 获取属性'y'
+print('obj.y =', obj.y) # 获取属性'y'
+
+print('getattr(obj, \'z\') =',getattr(obj, 'z', 404)) # 获取属性'z'，如果不存在，返回默认值404
+
+f = getattr(obj, 'power') # 获取属性'power'
+print(f)
+print(f())
+
+################################################################
+class Student(object):
+    name = 'student'
+
+s = Student()
+print(s.name)
+print(Student.name)
+s.name = 'Mark'
+print(s.name)
+print(Student.name)
+del s.name             # 如果删除实例的name属性
+print(s.name)
