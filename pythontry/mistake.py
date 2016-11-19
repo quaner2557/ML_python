5bef48e366d4cdc813a8fa99edf516b0caa2dfbb
quaner2557 <qq974807643@gmail.com>
2016-11-19 21:06:12 +0800

    some notes about debug

---
+++ pythontry/mistake.py
@@ -0,0 +1,67 @@
+try:
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+else:
+    print('no error!')
+finally:
+    print('finally...')
+print('END')
+
+def foo(s):
+    return 10 / int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        print('Error:', e)
+    finally:
+        print('finally...')
+
+main()
+
+# err.py:
+# def foo(s):
+#     return 10 / int(s)
+#
+# def bar(s):
+#     return foo(s) * 2
+#
+# def main():
+#     bar('0')
+#
+# main()
+
+###########################################################################
+# Python内置的logging模块可以非常容易地记录错误信息：
+import logging
+
+def foo(s):
+    return 10/int(s)
+
+def bar(s):
+    return foo(s) * 2
+
+def main():
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+
+main()
+print('END')
+
+###########################################################################
+# 抛出错误
+try:
+    10 / 0
+except ZeroDivisionError:
+    raise ValueError('input error!')
