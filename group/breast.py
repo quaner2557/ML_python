fae0b0df35b53894b2fe404c5c5b648264240ef2
quaner <qq974807643@gmail.com>
2017-04-07 17:46:40 +0800

    change into dataframe; outlier detection and  normalization

--- group/breast.py
+++ group/breast.py
@@ -7,6 +7,7 @@ Created on Thu Mar 23 15:32:24 2017
 
 import scipy.io as scio
 
+# 读取数据
 breast_train = scio.loadmat('E:/data/breastcancer/breast_train.mat')
 breast_test = scio.loadmat('E:/data/breastcancer/breast_test.mat')
 label_train = scio.loadmat('E:/data/breastcancer/label_train.mat')
@@ -17,29 +18,30 @@ breast_test =breast_test['breast_test']
 label_train = label_train['label_train']
 label_test = label_test['label_test']
 
+
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
-from sklearn.ensemble import ExtraTreesClassifier
-# Build a forest and compute the feature importances
-forest = ExtraTreesClassifier(n_estimators=10,
-                              random_state=0)
+# 转化为pandas数据类型
+breast_train = pd.DataFrame(breast_train,index=range(1,79),columns=range(1,24482))
+label_train = pd.DataFrame(label_train)
+breast_test = pd.DataFrame(breast_test,index=range(1,20),columns=range(1,24482))
+label_test = pd.DataFrame(label_test)
 
-forest.fit(breast_train, label_train)
-importances = forest.feature_importances_
-std = np.std([tree.feature_importances_ for tree in forest.estimators_],
-             axis=0)
-indices = np.argsort(importances)[::-1]
-
-# Print the feature ranking
-print("Feature ranking:")
+# 归一化
+from sklearn import preprocessing
 
 
-# Plot the feature importances of the forest
-plt.figure()
-plt.title("Feature importances")
-plt.bar(range(breast_train.shape[1]), importances[indices],
-       color="r", yerr=std[indices], align="center")
-plt.xticks(range(breast_train.shape[1]), indices)
-plt.xlim([-1, breast_train.shape[1]])
-plt.show()
+(breast_train-breast_train.mean)/breast_train.std > 3
+
+
+
+
+breast_train = preprocessing.normalize(breast_train.T)
+breast_train = breast_train.T
+breast_test = preprocessing.normalize(breast_test.T)
+breast_test = breast_test.T
+
+
+ 
