ed69444c58939ad3bfc1cd4fc86f79a3cee30593
quaner <qq974807643@gmail.com>
2017-04-14 13:41:58 +0800

    mrmr filter get the 50 features

--- group/mrmr filter.py
+++ group/mrmr filter.py
@@ -16,7 +16,7 @@ np.random.seed(0)
 MI_ff =np.zeros((500,500))
 for i in range(500):
     for j in np.arange(i,500):
-        MI_ff[i,j] = mutual_info_regression((breast_train_scaled_1.iloc[0:78,i]).reshape((78,1)),(breast_train_scaled_1.iloc[0:78,j]).reshape((78,1)))  
+        MI_ff[i,j] = mutual_info_regression((breast_train_scaled_1.iloc[0:78,i]).reshape((78,1)),(breast_train_scaled_1.iloc[0:78,j]).reshape((78,1)), n_neighbors=5)  
         if i % 10 ==0:
            print(i)
 
@@ -26,7 +26,7 @@ for i in range(499):
 
 
 # get the mutual information between features and label        
-MI_fl = mutual_info_regression(breast_train_scaled_1.iloc[0:78,:], label_train)
+MI_fl = mutual_info_regression(breast_train_scaled_1.iloc[0:78,:], label_train, n_neighbors=5)
 
 # the best 50 features
 candidate = np.zeros((500,1))     # 对应位置为1表示属于best 50
@@ -38,6 +38,14 @@ for i in range(50):
         mrmr = np.zeros((500,1))
         for j in range(500):
             mrmr[j] = (1-candidate[j])*(MI_fl[j]/i-np.sum(np.dot(MI_ff[j,:],candidate)/i**2))-1000*candidate[j]   # 1000*candidate[j]惩罚项
-        k = (np.where(mrmr == np.max(mrmr,axis=1)))[0]
+        k = (np.where(mrmr == np.max(mrmr,axis=0)))[0]
         candidate[k] = 1
 
+breast_train_50 = np.zeros((79,50))
+count = 0
+for i in range(500):
+    if candidate[i] == 1:
+        breast_train_50[:,count] = breast_train_scaled_1.iloc[:,i]
+        count += 1
+
+        
